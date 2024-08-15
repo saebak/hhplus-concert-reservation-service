c7ed37af3d0be1c6e905a14b9e39e8c092bbf4d9
saebak <okij3307@gmail.com>
2024-08-16 01:11:46 +0900

    outbox update 스케쥴러 작성

--- src/main/java/com/hhplus/backend/controller/payment/scheduler/PaymentScheduler.java
+++ src/main/java/com/hhplus/backend/controller/payment/scheduler/PaymentScheduler.java
@@ -1,11 +1,19 @@
 package com.hhplus.backend.controller.payment.scheduler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hhplus.backend.domain.payment.PaymentOutbox;
 import com.hhplus.backend.domain.payment.PaymentService;
-import com.hhplus.backend.domain.queue.QueueService;
+import com.hhplus.backend.support.client.PaymentMockApiClient;
+import com.hhplus.backend.support.enums.EventType;
+import com.hhplus.backend.support.event.PaymentPayload;
+import com.hhplus.backend.support.event.kafka.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -13,14 +21,19 @@ import org.springframework.stereotype.Component;
 public class PaymentScheduler {
 
     private final PaymentService paymentService;
+    private final KafkaProducer kafkaProducer;
+
+    private final PaymentMockApiClient paymentApiClient;
 
     @Scheduled(fixedRate = 5000)
-    public void runUpdatePaymentEvent() {
-//        paymentService.findAllByStatus(INIT).forEach {
-//            if(it.createdAt < now() - 5minutes) {
-//                slackApiClient.sendMessageToDeveloper();
-//                kafkaPublisher.publishOrderInfo();
-//            }
-//        }
+    public void runUpdatePaymentEvent() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String aggregateType = "paymentEvent";
+        List<PaymentOutbox> paymentOutboxs = paymentService.getPaymentOutboxsByStatusInit(aggregateType, "INIT");    // 쿼리로 시간 비교 , 실무에선 페이지네이션 필요(데이터 많으니깐)
+        for (PaymentOutbox paymentOutbox : paymentOutboxs) {
+            PaymentPayload payload = new PaymentPayload(paymentOutbox.getPayload());
+            paymentApiClient.sendPaymentInfo(payload);
+            kafkaProducer.publishPaymentInfo(paymentOutbox.getPayload());
+        }
     }
 }
