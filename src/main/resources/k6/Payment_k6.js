f5a444c89217efd4b61eb73d2a0f90c93056238d
saebak <okij3307@gmail.com>
2024-08-21 22:07:51 +0900

    테스트 스크립트 수정

--- src/main/resources/k6/Payment_k6.js
+++ src/main/resources/k6/Payment_k6.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 1,             /* 병렬로 while(true)를 얼마나 돌릴것인지 */
-    iterations: 1,     /* 각 사용자가 한번의 요청만 보냄 */
+    vus: 10,             /* 병렬로 while(true)를 얼마나 돌릴것인지 */
+    iterations: 10,     /* 각 사용자가 한번의 요청만 보냄 */
     thresholds: {
         http_req_failed: ['rate<0.1'], /* http error가 1% 이하 */
         http_req_duration: ['p(95)<200'], /* 95%의 요청이 200ms 아래 */
@@ -11,26 +11,26 @@ export const options = {
 };
 
 export default function () {
-     let url = 'http://192.168.0.139:8080/payment/pay';
-     let payload = JSON.stringify({
+    let url = 'http://192.168.0.139:8080/payment/pay';
+    let payload = JSON.stringify({
         "userId":1,
         "usePoint": 3000,
         "seatReservation": {
-            "id": 2,
+            "id": 1,
             "concertId": 1,
             "userId": 1,
             "scheduleId": 1,
             "seatId": 5
         }
-     });
+    });
 
-     let params = {
+    let params = {
         headers: {
             'Content-Type': 'application/json',
         },
-     }
-    
-     let res = http.post(url, payload, params);
+    }
+
+    let res = http.post(url, payload, params);
 
     check(res, {    /* 테스트 통과 조건 */
         'status is 200': (r) => r.status === 200,
--- src/main/resources/k6/Point_k6.js
+++ src/main/resources/k6/Point_k6.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 100,             /* 병렬로 while(true)를 얼마나 돌릴것인지 */
+    vus: 10,             /* 병렬로 while(true)를 얼마나 돌릴것인지 */
     duration: '5s',  // 1분 동안 테스트 실행
     thresholds: {
         http_req_failed: ['rate<0.1'], /* http error가 1% 이하 */
@@ -11,20 +11,20 @@ export const options = {
 };
 
 export default function () {
-     const userId = 1;
-     let url = 'http://192.168.0.139:8080/user/point/'+userId+'/charge';
-     let payload = 1000;
-     let params = {
+    const userId = 1;
+    let url = 'http://192.168.0.139:8080/user/point/'+userId+'/charge';
+    let payload = JSON.stringify(1000);;
+    let params = {
         headers: {
             'Content-Type': 'application/json',
         },
-     }
-    
-     let res = http.patch(url, payload, params);
+    }
+
+    let res = http.patch(url, payload, params);
 
     check(res, {    /* 테스트 통과 조건 */
         'status is 200': (r) => r.status === 200,
-        'correct points added': (r) => r.json().new_balance === 100, 
+        'correct points added': (r) => r.json().new_balance === 100,
     });
     sleep(1);
 }
